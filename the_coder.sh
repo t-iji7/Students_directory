cae46b72d3c6d3b0c5ac8e1ca56425068a6d5ee3
Oluwaloseyitowi Iji <o.iji@alustudent.com>
2025-01-29 12:02:06 +0200

    script new

--- the_coder.sh
+++ the_coder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #create directory called "students_directory"
-mkdir stud_dir
+mkdir students_directory
 
 # create 3 text files inside the students_directory
-touch stud_dir/cohort-1.txt
-touch stud_dir/cohort-2.txt
-touch stud_dir/cohort-3.txt
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
